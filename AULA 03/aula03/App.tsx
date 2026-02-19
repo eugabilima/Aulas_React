0f301d3b7b4db819a49aedbde7118b13309d6c8c
Gabriela Lima <gabirielaestudos010209@gmail.com>
2026-02-19 12:06:02 -0300

    app.tsx
    Initial implementation of the App component with a header.

---
+++ AULA 03/aula03/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>RECRIAR A INTERFACE COM NOME, E-MAIL PARA CONTATO, NOME DO GITHUB, USAR CORES DE FUNDO(CSS)</h1>
+    </> //fragment
+  )
+}
+
+export default App
